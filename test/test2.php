6717ee284338fc9da1642d988735c7a80cd0be2f
Liam <liam@kreator-inc.com>
2025-06-13 14:51:55 +0800

    feat(model): 增加属性获取的灵活性并优化模型关联- 在 Model 类的 __get 方法中增加对父类属性的获取，提高属性获取的灵活性 - 优化 StudentModel 和 StudentScoreModel 的关联关系定义，使用类名替代字符串 - 在测试文件中增加对模型关联数据的查询和输出，验证关联关系的正确性 - 调整测试文件中的代码结构，提高可读性和可维护性

--- test/test2.php
+++ test/test2.php
@@ -1,17 +1,17 @@
 <?php
 
+use Phalcon\Di\Di;
 use Phalcon\Di\FactoryDefault;
 
 require '../vendor/autoload.php';
+
 
 class StudentModel extends \Dm\PhalconOrm\model\Model
 {
     public function initialize(): void
     {
-        parent::initialize();
         $this->setSource('student');
-//        $this->hasMany("id", "StudentScoreModel", "student_id", ['alias' => 'scores']); // 修改为 "StudentScoreModel"
-        $this->hasMany("id", "StudentScoreModel", "student_id"); // 修改为 "StudentScoreModel"
+        $this->hasMany("id", StudentScoreModel::class, "student_id", ['alias' => 'scores']);
     }
 }
 
@@ -19,22 +19,22 @@ class StudentScoreModel extends \Dm\PhalconOrm\model\Model
 {
     public function initialize(): void
     {
-        parent::initialize();
-
         $this->setSource('student_score');
-        $this->belongsTo("student_id", "StudentModel", "id"
-        ); // 修改为 "StudentModel"
+        $this->belongsTo("student_id", StudentModel::class, "id", ['alias' => 'student']);
     }
 }
 
 // 模拟配置文件连接数据库
 $di = new FactoryDefault();
 // 必须显式注入服务
-$di->setShared('modelsManager', function () use ($di){
+$di->setShared('modelsManager', function () use ($di) {
     $manage = new \Phalcon\Mvc\Model\Manager();
     $manage->setDI($di);
     return $manage;
 });
+
+Di::setDefault($di);
+
 
 $mysql1 = $di->setShared('db', function () {
     $class = 'Dm\PhalconOrm\connector\Mysql';
@@ -51,15 +51,12 @@ $mysql1 = $di->setShared('db', function () {
     return new $class($params);
 });
 
-$data = StudentModel::whereIn("id",[1,2])->select();
-foreach ($data as $item) {
-    var_dump($item->getStudentScoreModel()->toArray());
+$db = new \Dm\PhalconOrm\DbManager();
+$db->setConnector($di->getShared('db'));
+
+$data = StudentModel::where("id", 1)->first();
+if ($data) {
+    var_dump($data->toArr());         // 查看主模型数据
+    var_dump($data->scores->toArray());  // 查看
+    //关联数据
 }
-//var_dump($data->readAttribute("id"));
-//var_dump($data->toArr());exit;
-//var_dump($data->getRelated('scores'),$data->scores);
-
-
-//var_dump($data->toArr(),$data->getStudentScoreModel());
-//var_dump($data->toArray(), $data->getStudentScoreModel()->toArray());
-exit;
